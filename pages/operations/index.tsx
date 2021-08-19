f92ee0bc98351f9602b24cb8672518b9d4a4d477
Thomas E Wilson <dev.thomas.e.wilson@gmail.com>
2021-08-18 22:07:32 -0500

    WIP: Adding Test page, modifying README.md

--- pages/operations/index.tsx
+++ pages/operations/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const operations: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -60,4 +60,4 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default operations
