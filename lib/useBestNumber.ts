c4bc6be5843bfbe2f7172ee8a1817813ee9cbe0c
Thomas E Wilson <dev.thomas.e.wilson@gmail.com>
2021-09-18 20:51:51 -0500

    Implemented useBestNumber Hook

--- lib/useBestNumber.ts
+++ lib/useBestNumber.ts
@@ -1,9 +1,28 @@
-// import type { BlockNumber } from '@polkadot/types/interfaces';
+import React, { useState } from 'react'
+import type { BlockNumber } from '@polkadot/types/interfaces';
 
-// import { useApi } from './useApi';
+import { useApi } from '/react-environment/state/modules/api/hooks';
+import { useSubscription } from '/lib'
+import { switchMap } from 'rxjs';
 
-export function useBestNumber (): number {
-//   const { api } = useApi();
-    return 5;
-//   return useCall<BlockNumber>(api.derive.chain.bestNumber);
+export function useBestNumber(): BlockNumber | undefined {
+    const api = useApi();
+    const [bestBlockNumber, setBestBlockNumber] = useState<BlockNumber>();
+
+    useSubscription(() => 
+        api.isReady
+            .pipe(
+                switchMap((api) => 
+                    api.derive.chain.bestNumberFinalized()
+                )
+            )
+            .subscribe({
+                next: (recentBlock) => setBestBlockNumber(recentBlock),
+                error: (e) => console.error(e),
+                complete: () => console.log('Event Complete: Switching Providers or Losing connection to Node')
+            }
+        ), [api]
+    )
+    return bestBlockNumber;
+
 }
--- pages/explorer/Events.tsx
+++ pages/explorer/Events.tsx
@@ -33,65 +33,8 @@ const TableCell = styled(Table.Cell)`
 // `;
 
 
-interface Props {
-  className?: string;
-  events?: KeyedEvent[];
-  eventClassName?: string;
-  label?: React.ReactNode;
-}
-
-function Events({ className = '', eventClassName, events, label }: Props): React.ReactElement<Props> {
-
-  const header = useMemo(() => [
-    [label || 'recent events', 'start']
-  ], [label]);
-
-  return (
-    <Table
-      className={className}
-    >
-      <Table.Header>
-        {header}
-      </Table.Header>
-      <Table.Body>
-        {events && events.map(({ blockHash, blockNumber, indexes, key, record }): React.ReactNode => (
-          <Table.Row
-            className={eventClassName}
-            key={key}
-          >
-            <Table.Cell className='overflow'>
-              <Event value={record} />
-              {blockNumber && (
-                <div className='event-link'>
-                  {indexes.length !== 1 && <span>({formatNumber(indexes.length)}x)&nbsp;</span>}
-                  <span>{formatNumber(blockNumber)}-{indexes[0]}</span>
-                </div>
-              )}
-            </Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
-  );
-}
-
-export default React.memo(styled(Events)`
-  td.overflow {
-    position: relative;
-
-    .event-link {
-      position: absolute;
-      right: 0.75rem;
-      top: 0.5rem;
-    }
-  }
-`);
-
-
-
-
-
-<Row gutter={[24, 25]}>
+/* 
+  <Row gutter={[24, 25]}>
   <Col span={24}>
     <Card variant='gradient-border'>
       <Card.Header>
@@ -157,3 +100,58 @@ export default React.memo(styled(Events)`
     </Card>
   </Col>
 </Row>
+*/
+
+interface Props {
+  className?: string;
+  events?: KeyedEvent[];
+  eventClassName?: string;
+  label?: React.ReactNode;
+}
+
+function Events({ className = '', eventClassName, events, label }: Props): React.ReactElement<Props> {
+
+  const header = useMemo(() => [
+    [label || 'recent events', 'start']
+  ], [label]);
+
+  return (
+    <Table
+      className={className}
+    >
+      <Table.Header>
+        {header}
+      </Table.Header>
+      <Table.Body>
+        {events && events.map(({ blockHash, blockNumber, indexes, key, record }): React.ReactNode => (
+          <Table.Row
+            className={eventClassName}
+            key={key}
+          >
+            <Table.Cell className='overflow'>
+              <Event value={record} />
+              {blockNumber && (
+                <div className='event-link'>
+                  {indexes.length !== 1 && <span>({formatNumber(indexes.length)}x)&nbsp;</span>}
+                  <span>{formatNumber(blockNumber)}-{indexes[0]}</span>
+                </div>
+              )}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
+  );
+}
+
+export default React.memo(styled(Events)`
+  td.overflow {
+    position: relative;
+
+    .event-link {
+      position: absolute;
+      right: 0.75rem;
+      top: 0.5rem;
+    }
+  }
+`);
